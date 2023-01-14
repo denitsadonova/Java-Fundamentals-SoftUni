94e9ed887db7b0c8e0abac05578f6dd1fa7bc6b3
denitsadonova <denitsadonova@gmail.com>
2023-01-14 13:54:29 +0000

    Create Print_Part_Of_Ascii_Table.java

---
+++ Data_Types_And_Variables_Excercise/Print_Part_Of_Ascii_Table.java
@@ -0,0 +1,16 @@
+package Data_Types_Excercise;
+
+import java.util.Scanner;
+
+public class Print_Part_Of_Ascii_Table {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startCode = Integer.parseInt(scanner.nextLine());
+        int endCode = Integer.parseInt(scanner.nextLine());
+
+        for (int code = startCode; code <= endCode; code++) {
+            System.out.print((char) code + " ");
+        }
+    }
+}
