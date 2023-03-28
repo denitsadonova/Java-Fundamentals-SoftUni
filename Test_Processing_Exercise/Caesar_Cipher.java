1c06e429034081bf610730643e8d341a4662b32b
denitsadonova <denitsadonova@gmail.com>
2023-03-28 12:46:05 +0100

    Create Caesar_Cipher.java

---
+++ Test_Processing_Exercise/Caesar_Cipher.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Caesar_Cipher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (char symbol : text.toCharArray()) {
+            char encryptSymbol = (char)(symbol + 3);
+            encryptedText.append(encryptSymbol);
+        }
+
+        System.out.println(encryptedText);
+    }
+}
